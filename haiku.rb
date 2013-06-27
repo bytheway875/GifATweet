0d211a78af3e02fa9193a9749fc1e3cef66b02e6
MakerSquare Student <student@themakersquare.com>
2013-06-27 11:53:13 -0500

    Removed stop words from tweets.  Added GIF

--- haiku.rb
+++ haiku.rb
@@ -1,9 +1,11 @@
-require './syllable_count'
+require './remove_stop_words'
 require 'twitter'
-require 'flickraw'
 require 'launchy'
 require 'instagram'
 require 'sinatra'
+require 'net/http'
+require 'json'
+
 
 
 Twitter.configure do |config|
@@ -13,10 +15,14 @@ Twitter.configure do |config|
   config.oauth_token_secret = ENV["OAUTH_TOKEN_SECRET"]
 end
 
- tweets = Twitter.search("#micropoetry", count:10, language:'en').results
+ tweets = Twitter.user_timeline("s_byrne", language:'en')
 rand = Random.new.rand(tweets.length)
 
-tweet_text = tweets[rand].text.gsub!(/http\S*\b|@\S*\b|\bRT:?\b|/,'')
+tweet_text = tweets[rand].text.gsub(/http\S*\b|@\S*\b|\bRT:?\b|/,'')
+tweet_text_keywords = remove_stop_words(tweet_text)
+puts tweet_text
+puts
+puts tweet_text_keywords.inspect
 
 # tweets.map do |tweet|
 #  tweet.text.gsub!(/http\S*\b|@\S*\b|\bRT:?\b|/,'')#.syllable_count == 17  #removes anything starting with http
@@ -46,46 +52,71 @@ tweet_text = tweets[rand].text.gsub!(/http\S*\b|@\S*\b|\bRT:?\b|/,'')
 # Launchy.open(image)
 
 
+
+
+
+rand_keyword_1 = Random.new.rand(tweet_text_keywords.length-1)
+rand_keyword_2 = Random.new.rand(tweet_text_keywords.length-1)
+  while rand_keyword_1 == rand_keyword_2
+    rand_keyword_2 = Random.new.rand(tweet_text_keywords.length-1)
+  end
+
+puts tweet_text_keywords[rand_keyword_1]
+puts tweet_text_keywords[rand_keyword_2]
+
+
+url = "http://api.giphy.com/v1/gifs/search?q=#{tweet_text_keywords[rand_keyword_1]}-#{tweet_text_keywords[rand_keyword_2]}&api_key=dc6zaTOxFJmzC&limit=5"
+resp = Net::HTTP.get_response(URI.parse(url))
+buffer = resp.body
+result = JSON.parse(buffer)
+
+while result["data"]==[]
+  rand_keyword_1 = Random.new.rand(tweet_text_keywords.length-1)
+  rand_keyword_2 = Random.new.rand(tweet_text_keywords.length-1)
+  while rand_keyword_1 == rand_keyword_2
+    rand_keyword_2 = Random.new.rand(tweet_text_keywords.length-1)
+  end
+
+puts tweet_text_keywords[rand_keyword_1]
+puts tweet_text_keywords[rand_keyword_2]  
+
+url = "http://api.giphy.com/v1/gifs/search?q=#{tweet_text_keywords[rand_keyword_1]}-#{tweet_text_keywords[rand_keyword_2]}&api_key=dc6zaTOxFJmzC&limit=5"
+resp = Net::HTTP.get_response(URI.parse(url))
+buffer = resp.body
+result = JSON.parse(buffer)
+
+end
+
+result_url = result["data"][0]["images"]["original"]["url"]
+
+# Launchy.open(result)
+
 enable :sessions
 
 CALLBACK_URL = "http://localhost:4567/oauth/callback"
 
-Instagram.configure do |config|
-  config.client_id = ENV["INSTA_CLIENT_ID"]
-  config.client_secret = ENV["INSTA_CLIENT_SECRET"]
-end
+# Instagram.configure do |config|
+#   config.client_id = ENV["INSTA_CLIENT_ID"]
+#   config.client_secret = ENV["INSTA_CLIENT_SECRET"]
+# end
 
-results = Instagram.tag_recent_media('nintendo', 'mario', options = {count: 10})
-rand = Random.new.rand(results.length)
-  image = results[rand].images.low_resolution.url
-  tags = results[rand].tags
-  user = results[rand].user.username
+# results = Instagram.tag_recent_media('nintendo', 'mario', options = {count: 10})
+# rand = Random.new.rand(results.length)
+#   image = results[rand].images.low_resolution.url
+#   tags = results[rand].tags
+#   user = results[rand].user.username
 
 get "/" do
   '<a href="/oauth/connect">Connect with Instagram</a>'
-  html = "<img src='#{image}'>"
-  html << "<h2>#{tags}</h2>"
-  html << "<h2>#{user}</h2>"
-  html << "<h2>#{tweet_text}</h2>"
+  html = "<img src=#{result_url} alt='Gosling' height='300'>"
+  # html << "<h2>#{tags}</h2>"
+  # html << "<h2>#{user}</h2>"
+  # html << "<h2>#{tweet_text}</h2>"
 
-  html
+  html << "<h2>#{tweets[rand].text}</h2>"
+  html << "<h2>#{tweet_text_keywords}</h2>"
+  html << "<h1>#{tweet_text_keywords[rand_keyword_1]}   #{tweet_text_keywords[rand_keyword_2]}"
+  html << "<h2>#{JSON.parse(buffer)["data"][0]["url"]}</h2>"
 end
 
-get "/oauth/connect" do
-  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
-end
 
-get "/oauth/callback" do
-  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
-  session[:access_token] = response.access_token
-  redirect "/feed"
-end
-
-get "/feed" do
-  client = Instagram.client(:access_token => session[:access_token])
-  user = client.user
-
-  html = "<h1>Test recent photos</h1>"
-  html << "<p>This is a test</p>"
-  html
-end
